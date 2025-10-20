23ad2930d5dd70acfe337bb36c60c6092ccc71ea
seb <sebgonz@sebs.com>
2025-10-20 14:46:44 -0500

    Estructura principal del Proyecto

---
+++ Lament-Of-Ashes/src/main/java/com/mycompany/lament/of/ashes/LamentOfAshes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lament.of.ashes;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LamentOfAshes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
